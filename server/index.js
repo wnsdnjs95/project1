671e1c72706b2516ffb532e9016d8f6207dc85af
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-07-17 14:50:52 +0900

    몽고DB 모델과 스키마 만들기

--- server/index.js
+++ server/index.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `mongodb+srv://wnsdnjs95:wnsdnjs12!@@cluster123.651dw.mongodb.net/<dbname>?retryWrites=true&w=majority
+`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// mongodb+srv://wnsdnjs95:<password>@cluster123.651dw.mongodb.net/<dbname>?retryWrites=true&w=majority
