84d17c99df79f9bb4960945cff48da98b349dd2f
junwon95 <wnsdnjs95@users.noreply.github.com>
2020-07-17 16:52:41 +0900

    nodemon 설치
    node 서버를 끄고 닫는것이 불편해서
    nodemon이라는 dependeny를 이용해 코드를 변경해도 서버를 끄지 않고 리로드만으로 변경된 내용을 바로 적용할 수 있다

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 데이터들을 분석
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 데이터들을 분석
+app.use(bodyParser.json());
+
 mongoose
   .connect(
     `mongodb+srv://wnsdnjs95:wnsdnjs12!@@cluster123.651dw.mongodb.net/<dbname>?retryWrites=true&w=majority
@@ -17,5 +25,23 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!123"));
+
+// 회원 가입을 위한 route
+app.post("/api/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다
+
+  // bodyParser dependency를 이용해 req.body로 클라이언트에 보내는 정보를 받아준다
+
+  const user = new User(req.body);
+
+  user.save((err, usrInfo) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
