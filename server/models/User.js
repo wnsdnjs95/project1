adcfbc30a98501ade2859e02503297af395a1ef0
wnsdnjs95 <43410943+wnsdnjs95@users.noreply.github.com>
2020-07-20 16:40:33 +0900

    bcrypt로 비밀번호 암호화하기

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// 10자리인 salt 생성
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema(
   {
@@ -29,6 +33,25 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  //비밀번호를 바꿀때만 암호화 요청
+  if (user.isModified("password")) {
+    // salt 를 이용해서 비밀번호를 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        // hash는 암호된 비밀번호
+        next();
+      });
+    });
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 
